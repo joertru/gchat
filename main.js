832d0551bda1a1b13d12d7b1287d7052bf24a02f
Jorge E Trujillo F <info@joertru.com>
2020-06-15 16:49:45 -0500

    Ajuste de compilación mac, Ajuste systray, add icon

--- main.js
+++ main.js
@@ -1,46 +1,55 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray, Menu} = require('electron')
 const path = require('path')
+const iconpath = path.join(__dirname, 'icon.png')
+let appIcon = null
+let win = null
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+app.whenReady().then(() => {
+
+  win = new BrowserWindow({
     width: 1024,
     height: 768,
     title: "Google Chat Client",
+    icon: iconpath,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
- 
-  // and load the index.html of the app.
-  mainWindow.loadURL('https://chat.google.com')
-  mainWindow.setMenu(null)
 
- 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+  win.loadURL('https://chat.google.com')
+  win.setMenu(null)
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-   
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+  appIcon = new Tray(iconpath)
+
+  var MenuTray = Menu.buildFromTemplate([
+    { label: 'Mostrar', click:  function(){
+        win.show();
+      } },
+    { label: 'Cerrar', click:  function(){
+        app.isQuiting = true;
+        app.quit();
+      } }
+  ]);
+
+  appIcon.setToolTip("Google Chat Client");
+  appIcon.setContextMenu(MenuTray);
+
+  win.on('minimize',function(event){
+    event.preventDefault();
+    win.hide();
+  });
+
+  win.on('close', function (event) {
+    if(!app.isQuiting){
+      event.preventDefault();
+      win.hide();
+    }
+    return false;
+  });
+
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
